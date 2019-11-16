b91e1c45d7221d2af11938b559d3e52bbfdc8ecd
Ivan Dimitrov <33979946+ivdim1999@users.noreply.github.com>
2019-11-17 01:16:03 +0200

    Added link checker
    Checks links for invalid formats

--- server/image_initializer.py
+++ server/image_initializer.py
@@ -1,8 +1,17 @@
 import azure_text_fetcher
 import text_blob_classifier
+import numpy as np
+
+SUPPORTED_FORMATS = ['.jpg', '.jpeg', '.png', '.bmp']
+
+
+def check_link(image_url):
+    global SUPPORTED_FORMATS
+    return np.any([(image_format in image_url) for image_format in SUPPORTED_FORMATS])
 
 
 def initialize_images(image_urls):
     blob_list_list = [
-        text_blob_classifier.analyze_image(azure_text_fetcher.analyze_text(image_url)) for image_url in image_urls]
+        text_blob_classifier.analyze_image(azure_text_fetcher.analyze_text(image_url))
+        for image_url in image_urls if check_link(image_url)]
     return blob_list_list
